f9a2ab26a80f4c41e169368fd6585f1a803000f8
Milena Mocci <miliscm@gmail.com>
2020-07-06 10:34:00 -0300

    iniciando desafio 4.1

--- aulas-particulares/routes.js
+++ aulas-particulares/routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/", function (res, req) {
+routes.get("/", function (req, res) {
   return res.redirect("/teachers");
 });
 routes.get("/teachers", function (req, res) {
-  return res.render("/teachers");
+  return res.render("teachers/index");
 });
-routes.get("/students", function () {
+routes.get("/students", function (req, res) {
   return res.render("students");
 });
 
